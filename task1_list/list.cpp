ecf97ff9c1c20e9d07424fadc48a76702e0e4815
Mateusz1477 <101249456+Mateusz1477@users.noreply.github.com>
2022-03-16 20:51:34 +0100

    Update list.cpp

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -1,13 +1,32 @@
 #include "pch.h"
 #include "list.h"
+#include "iostream"
 
-
+using  namespace std;
 
 List* initList(void)
 {
-	List* pRoot = (List*)malloc(sizeof(*pRoot));
-	
+List* pRoot = (List*)malloc(sizeof(*pRoot));
+
+	if (pRoot != NULL)
+	{
+	pRoot->pNext = NULL;
+	cout << "Nadanie pamieci " << endl;
+	}
+	else
+	{
+	if (pRoot == NULL)
+
+	{
+	cout << "problem z przydzieleniem pamieci" << endl;
+	}
+	else
+	{
+	cout << "błąd-brak rozwiązania " << endl;
+	}
+
 	return pRoot;
+	}
 }
 
 void deinitList(List* pRoot)
@@ -15,20 +34,14 @@ void deinitList(List* pRoot)
 	free(pRoot);
 	pRoot = NULL;
 }
-
 void addCar(List* pRoot, char* plateNum)
 {
-
 }
-
 void removeCar(List* pRoot, char* plateNum)
 {
-
 }
 void printAll(List* pRoot) {
-
 }
-
 int numberOfElements(List* pRoot)
 {
 	return 0;
