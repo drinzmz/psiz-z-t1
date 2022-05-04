fd2b769de8ca87d3468f09e89883dae94806839f
polkm2000 <101151328+polkm2000@users.noreply.github.com>
2022-05-04 21:52:03 +0200

    Update list.cpp

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -35,9 +35,10 @@ void deinitList(List* pRoot)
 	
 }
 
-void addCar(List* pRoot, char* plateNum)
+void addCar(List* pRoot, char* plateNum) //Function add car
 {
 }
+
 
 void removeCar(List* pRoot, char* plateNum)
 {
